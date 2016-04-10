457a84ee11def76cf4d17d045b9c32a3a877760d
HongxuChen <leftcopy.chx@gmail.com>
2016-04-11 02:28:02 +0800

    014

--- src/main/scala/euler/a1/Euler014.scala
+++ src/main/scala/euler/a1/Euler014.scala
@@ -1,43 +1,61 @@
 package euler
 package a1
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 object Euler014 extends App {
+  type KeyTy = Long
+  type ValueTy = Int
+  type MapTy = mutable.Map[KeyTy, ValueTy]
+  type EleTy = (Int, ValueTy)
 
-  def getChain(n: Int) = {
-    var current = n
-    var counter = 1
-    var buf = mutable.ArrayBuffer(current)
-    while (current != 1) {
-      current = if (current % 2 == 0) current / 2 else 3 * current + 1
-      buf += current
-      counter += 1
+  def f1(n: Int): Int = {
+
+    @inline
+    def getChain(n: Int, m: MapTy): EleTy = {
+      var current = n.toLong
+      var buf = mutable.ArrayBuffer.empty[KeyTy]
+      do {
+        buf += current
+        current = if (current % 2 == 0) current / 2 else 3 * current + 1
+      } while (!m.contains(current))
+      val knownCounter = m(current)
+      val len = buf.length
+      for (i <- buf.indices) {
+        m += buf(i) -> (knownCounter + len - i)
+      }
+      n -> (knownCounter + len)
     }
-    buf
-  }
 
-  def f(n: Int) = {
-    val counters = Array.ofDim[Int](n)
-    for (i <- 1 to n) {
-      if (counters(i - 1) == 0) {
-        val buf = getChain(i)
-        //        println(s"$i, $buf")
-        for (j <- buf.indices) {
-          val number = buf(j)
-          if (number < counters.length) {
-            counters(number - 1) = buf.length - j
-          }
+    val m = mutable.Map[KeyTy, ValueTy](1L -> 1)
+    var res: (Int, ValueTy) = (0, 0)
+    for (i <- 2 to n) {
+      if (!m.contains(i)) {
+        val cur = getChain(i, m)
+        if (res._2 < cur._2) {
+          res = cur
         }
       }
     }
-
+    res._1
   }
 
-  println(getChain(6))
-  println(getChain(16))
+  def f2(n: Int): Int = {
+    @tailrec
+    def from(n: Long, c: Int = 0): Int = {
+      n match {
+        case 1 => c + 1
+        case _ => from(if (n % 2 == 0) n / 2 else 3 * n + 1, c + 1)
+      }
+    }
+
+    (1 until n).view.map(n => (n, from(n)))
+      .reduceLeft((a, b) => if (a._2 > b._2) a else b)._1
+  }
+
   val n = 1000000
-  //  val n = 20
-  println(f(n))
+  //  pTimeIt(f1(n), seconds = 1, round = 2, chunk = 2)
+  pTimeIt(f2(n), seconds = 1, round = 2, chunk = 2)
 
 }
