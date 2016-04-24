2274e01634afaed9659f34d7f4b62ced33dcd84c
HongxuChen <leftcopy.chx@gmail.com>
2016-04-24 22:07:55 +0800

    e24

--- src/main/scala/euler/a2/Euler024.scala
+++ src/main/scala/euler/a2/Euler024.scala
@@ -1,8 +1,32 @@
 package euler.a2
 
+import scala.collection.mutable
+import scala.util.control.Breaks._
+
 object Euler024 extends App {
 
   def f(n: Int) = {
+    val arr = Array.range(9, 0, -1).scanRight(1)(_ * _)
+    val sb = new mutable.StringBuilder
+    val lb = mutable.ListBuffer.range(0, 10)
+    var remaining = n + 1
+    breakable {
+      for (i <- arr.indices) {
+        val curFact = arr(i)
+        val j = remaining / curFact
+        remaining = remaining - curFact * j
+        val removed = lb.remove(j)
+        sb.append(removed)
+        if (remaining == 0) break
+      }
+    }
+    sb.append(lb.mkString)
+    sb.toString
   }
+
+  import utils.Bench._
+
+  val num = 1000000
+  pTimeIt(f(num))
 
 }
